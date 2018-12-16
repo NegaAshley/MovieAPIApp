f6a3a03cf14f03fe1a2ca5d41ad1b9e72cc99c78
NegaAshley <ashleymxu@gmail.com>
2018-12-16 21:28:37 +0000

    Showing list of movie with search 'Indiana'

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+app.set("view engine", "ejs");
 
-//Root route
+//Results route
 app.get("/results", function(req, res){
     console.log("Results accessed");
     request("http://www.omdbapi.com/?s=indiana&apikey=thewdb", function(error, response, body){
         if(!error && response.statusCode == 200){
-            var results = JSON.parse(body);
-            res.send(results["Search"][0]);
+            var data = JSON.parse(body);
+            res.render("results", {data: data});
         }
     });
 });
