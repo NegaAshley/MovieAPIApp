407e64bf9d35713bf04ec7e07795170e4559d2c2
NegaAshley <ashleymxu@gmail.com>
2018-12-16 21:48:08 +0000

    Add search functionality, year, and back link

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ var app = express();
 var request = require("request");
 app.set("view engine", "ejs");
 
-//Results route
+app.get("/",  function(req, res){
+    console.log("Root route accessed")
+    res.render("search");
+});
+
 app.get("/results", function(req, res){
     console.log("Results accessed");
-    request("http://www.omdbapi.com/?s=indiana&apikey=thewdb", function(error, response, body){
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+    request(url, function(error, response, body){
         if(!error && response.statusCode == 200){
             var data = JSON.parse(body);
             res.render("results", {data: data});
